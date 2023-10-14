fd5cbcc4be2e1a48bc287f6f19504d12a6906b78
KoshalSain7 <140868383+KoshalSain7@users.noreply.github.com>
2023-10-14 22:47:08 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,12 +1,7 @@
 const mongoose = require("mongoose");
-const Detail = require("../models/detail");
-const Slider = require("../models/slider");
-const Service = require("../models/service");
-const banner2 = require("../models/banner2");
-const banner3 = require("../models/banner3");
-const Registers = require("../models/registers");
 
-const dbUrl = "mongodb+srv://koshal777:Pass777@cluster1d.jrgwv9b.mongodb.net/?retryWrites=true&w=majority";
+
+const dbUrl = process.env.MONGO_URL
 
 mongoose.connect(dbUrl, {
     dbName: "website_tut",
